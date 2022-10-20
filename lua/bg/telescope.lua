1d72dda2e891c1edfbbe6ef0bf6a8ca5a688f796
Bryan Grohman <bgrohman@fastmail.com>
2022-10-20 10:51:25 -0500

    More telescope key mappings

--- lua/bg/telescope.lua
+++ lua/bg/telescope.lua
@@ -4,3 +4,9 @@ vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
 vim.keymap.set('n', 'gs', builtin.grep_string, {})
+vim.keymap.set('n', 'gc', builtin.git_bcommits, {})
+vim.keymap.set('n', 'ts', builtin.treesitter, {})
+vim.keymap.set('n', 'fz', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', 'tr', builtin.resume, {})
+-- TODO What keymap for builtin.git_status?
+-- TOOD consider: marks, loclist, quickfix, registers, lsp stuff
