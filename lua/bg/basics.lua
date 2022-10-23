47edf77d852db285f7e3f23d7a3381d78da64b02
Bryan Grohman <bgrohman@fastmail.com>
2022-10-22 23:13:40 -0500

    formatting

--- lua/bg/basics.lua
+++ lua/bg/basics.lua
@@ -12,7 +12,7 @@ vim.cmd.filetype('plugin on')
 vim.o.backup = false
 vim.o.swapfile = false
 vim.o.undofile = true
-vim.o.history=10000
+vim.o.history = 10000
 vim.o.wrap = false
 vim.o.number = true
 vim.o.relativenumber = true
