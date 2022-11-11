3f29ad7d107aaf6105ce32ec77b1d09aa039bae7
Bryan Grohman <bgrohman@fastmail.com>
2022-11-11 07:56:46 -0600

    added git auto commit command

--- lua/bg/git.lua
+++ lua/bg/git.lua
@@ -1,2 +1,10 @@
 require('neogit').setup()
 require('gitsigns').setup()
+
+local gitAutoCommit = function()
+    local date = os.date('%c')
+    local command = '!git add . ; git commit -m "auto commit ' .. date .. '"'
+    vim.api.nvim_exec(command, false)
+end
+
+vim.api.nvim_create_user_command('GitAutoCommit', gitAutoCommit, {desc = 'Automatically commit all modified files'})
